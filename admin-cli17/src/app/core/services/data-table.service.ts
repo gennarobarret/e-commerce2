353c9662e2a38774be7abf54fd477fa2e657cd5a
Javier <jfbgennaro@gmail.com>
2024-06-29 21:46:06 +0200

    notifications

--- admin-cli17/src/app/core/services/data-table.service.ts
+++ admin-cli17/src/app/core/services/data-table.service.ts
@@ -1,21 +1,58 @@
 import { Injectable } from '@angular/core';
-import { DataTable } from 'simple-datatables';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataTableService {
-  datatable: DataTable | null = null;
+  constructor() { }
 
-  initializeTable(tableId: string): void {
-    const tableElement = document.getElementById(tableId);
-    if (tableElement) {
-      this.datatable = new DataTable(tableElement as HTMLTableElement, {
-        searchable: true,
-        fixedHeight: true,
-        perPage: 10,
-        perPageSelect: [5, 10, 15, 20],
-      });
-    }
+  // Método para filtrar los datos
+  applyFilter<T>(items: T[], filterKey: string, filterValue: string): T[] {
+    return items.filter(item =>
+      (item as any)[filterKey].toString().toLowerCase().includes(filterValue.toLowerCase())
+    );
   }
+
+  // Método para paginar los datos
+  paginate<T>(items: T[], pageSize: number, page: number): T[] {
+    const start = (page - 1) * pageSize;
+    return items.slice(start, start + pageSize);
+  }
+
+  // Método para ordenar los datos
+  sort<T>(items: T[], key: string, sortOrder: 'asc' | 'desc'): T[] {
+    return items.sort((a, b) => {
+      let valueA = (a as any)[key];
+      let valueB = (b as any)[key];
+
+      // Verificar si los valores son nulos o indefinidos
+      if (valueA == null) valueA = '';
+      if (valueB == null) valueB = '';
+
+      // Manejar valores numéricos
+      if (typeof valueA === 'number' && typeof valueB === 'number') {
+        return sortOrder === 'asc' ? valueA - valueB : valueB - valueA;
+      }
+
+      // Manejar fechas
+      if (valueA instanceof Date && valueB instanceof Date) {
+        valueA = valueA.getTime();
+        valueB = valueB.getTime();
+        return sortOrder === 'asc' ? valueA - valueB : valueB - valueA;
+      }
+
+      // Convertir valores a cadenas para compararlos
+      valueA = valueA.toString().toLowerCase();
+      valueB = valueB.toString().toLowerCase();
+
+      if (valueA < valueB) {
+        return sortOrder === 'asc' ? -1 : 1;
+      } else if (valueA > valueB) {
+        return sortOrder === 'asc' ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
 }
--- admin-cli17/src/app/features/notifications/list-notifications/list-notifications.component.ts
+++ admin-cli17/src/app/features/notifications/list-notifications/list-notifications.component.ts
@@ -1,45 +1,49 @@
-import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { FeatherIconsService } from '../../../core/services/feather-icons.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { DataTableService } from '../../../core/services/data-table.service';
-import { FeatherIconsService } from '../../../core/services/feather-icons.service';
 import { NotificationService } from '../../../core/services/notifications.service';
 import { SocketService } from '../../../core/services/socket.service';
+import { DataTableService } from '../../../core/services/data-table.service';
 import { Notification } from '../../../core/models/notification.model';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list-notifications',
-  templateUrl: './list-notifications.component.html',
-  styleUrls: ['./list-notifications.component.css'],
   standalone: true,
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule],
+  templateUrl: './list-notifications.component.html',
+  styleUrls: ['./list-notifications.component.css']
 })
 export class ListNotificationsComponent implements OnInit, AfterViewInit, OnDestroy {
   notifications: Notification[] = [];
+  filteredNotifications: Notification[] = [];
   private socketSubscription: Subscription | null = null;
+  page = 1;
+  pageSize = 10;
+  pageSizeOptions = [5, 10, 15, 20, 25];
+  sortOrder: 'asc' | 'desc' = 'asc';
+  sortKey: string = 'message'; // Definir sortKey
+  load_data = true;
+  selectedFilterKey: string = 'message';
+  searchText: string = '';
+  filterKeys: string[] = ['message', 'date', 'isViewed'];
 
   constructor(
-    private dataTableService: DataTableService,
-    private featherIconsService: FeatherIconsService,
     private notificationService: NotificationService,
-    private socketService: SocketService
+    private socketService: SocketService,
+    private featherIconsService: FeatherIconsService,
+    private dataTableService: DataTableService
   ) { }
 
   ngOnInit(): void {
-    this.notificationService.getNotifications().subscribe(
-      (response: any) => {
-        this.notifications = response.data.notifications;
-      },
-      error => {
-        console.error('Error fetching notifications', error);
-      }
-    );
+    this.fetchNotifications();
 
     this.socketSubscription = this.socketService.on('notification').subscribe(
       (notification: Notification) => {
         if (!this.notifications.some(notif => notif._id === notification._id)) {
           this.notifications.unshift(notification);
+          this.applyFilter();
           this.activateFeatherIcons();
         }
       }
@@ -47,10 +51,25 @@ export class ListNotificationsComponent implements OnInit, AfterViewInit, OnDest
   }
 
   ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.dataTableService.initializeTable('notifications-table');
-      this.activateFeatherIcons();
-    }, 0);
+    this.activateFeatherIcons();
+  }
+
+  fetchNotifications(): void {
+    this.notificationService.getNotifications().subscribe(
+      (response: any) => {
+        this.notifications = response.data.notifications;
+        this.applyFilter();
+        this.load_data = false;
+        this.activateFeatherIcons();
+      },
+      (error: any) => {
+        console.error('Error fetching notifications', error);
+      }
+    );
+  }
+
+  activateFeatherIcons(): void {
+    this.featherIconsService.activateFeatherIcons();
   }
 
   ngOnDestroy(): void {
@@ -59,14 +78,17 @@ export class ListNotificationsComponent implements OnInit, AfterViewInit, OnDest
     }
   }
 
+
   markAsViewed(notification: Notification): void {
     this.notificationService.markAsViewed(notification._id).subscribe(
       () => {
         this.notifications = this.notifications.map(notif =>
           notif._id === notification._id ? { ...notif, isViewed: true } : notif
         );
+        this.applyFilter();
+        this.activateFeatherIcons();
       },
-      error => {
+      (error: any) => {
         console.error('Error marking notification as viewed', error);
       }
     );
@@ -76,14 +98,58 @@ export class ListNotificationsComponent implements OnInit, AfterViewInit, OnDest
     this.notificationService.deleteNotification(notificationId).subscribe(
       () => {
         this.notifications = this.notifications.filter(notif => notif._id !== notificationId);
+        this.applyFilter();
+        this.activateFeatherIcons();
       },
-      error => {
+      (error: any) => {
         console.error('Error deleting notification', error);
       }
     );
   }
 
-  activateFeatherIcons(): void {
-    this.featherIconsService.activateFeatherIcons();
+  sort(key: string) {
+    this.sortKey = key; // Actualizar sortKey
+    this.notifications = this.dataTableService.sort(this.notifications, key, this.sortOrder);
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    this.filteredNotifications = this.dataTableService.applyFilter(this.notifications, this.selectedFilterKey, this.searchText);
+    this.filteredNotifications = this.dataTableService.paginate(this.filteredNotifications, this.pageSize, this.page);
+  }
+
+  search() {
+    this.applyFilter();
+  }
+
+  reset() {
+    this.searchText = '';
+    this.applyFilter();
+  }
+
+  onPageSizeChange(event: any) {
+    this.pageSize = +event.target.value;
+    this.page = 1;
+    this.applyFilter();
+  }
+
+  getRowClass(type: string): string {
+    switch (type) {
+      case 'success':
+        return 'table-success';
+      case 'info':
+        return 'table-info';
+      case 'warning':
+        return 'table-warning';
+      case 'danger':
+        return 'table-danger';
+      default:
+        return '';
+    }
+  }
+
+  getTotalPages(): number {
+    return Math.ceil(this.filteredNotifications.length / this.pageSize);
   }
 }
